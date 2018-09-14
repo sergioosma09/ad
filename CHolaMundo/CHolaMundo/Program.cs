39d41a37aaf8afde72f3b785e9c784aa095aa25f
sergioosma09 <sergioosma09@gmail.com>
2018-09-14 09:19:38 +0200

    Añadidos MySql.Data.dll CHolaMundo y el CMySql

---
+++ CHolaMundo/CHolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CHolaMundo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.Write("Hello World");
+        }
+    }
+}
