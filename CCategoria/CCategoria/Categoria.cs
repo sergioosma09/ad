459fd9a345390aafaf93a0a650e04792d613d5a4
sergioosma09 <sergioosma09@gmail.com>
2018-09-21 11:23:23 +0200

    Actualizado CCategoria

--- CCategoria/CCategoria/Categoria.cs
+++ CCategoria/CCategoria/Categoria.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 namespace CCategoria
 {
-    public class Categoria
-    {
-        public Categoria()
+	public class Categoria
+	{
+		private ulong id;
+		private string nombre;
+
+		public Categoria()
+		{
+		}
+		public Categoria(ulong id, string nombre)
+		{
+			this.id = id;
+			this.nombre = nombre;
+		}
+		public ulong Id
+		{
+			get { return id; }
+			set{id=value;}
+
+		}
+		public string Nombre
         {
+			get { return nombre; }
+			set { nombre = value; }
+
         }
+
     }
 }
--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -3,45 +3,80 @@ using Gtk;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System.Collections.Generic;
-
+using CCategoria;
+using System.Reflection;
 
 public partial class MainWindow : Gtk.Window
 {
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
+		IDbConnection dbConnection = new MySqlConnection(
+                "server=localhost; database=dbprueba;user=root;password=sistemas;ssl-mode=none"
+            );
+        dbConnection.Open();
+		IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = "select id,nombre from categoria order by id";
+        IDataReader dataReader = dbCommand.ExecuteReader();
 
-		object obj = null;
-		Console.WriteLine("" + obj);
-		Console.WriteLine("Fin");		                  
 
-		//IDbConnection dbConnection = new MySqlConnection(
-  //              "server=localhost; database=dbprueba;user=root;password=sistemas;ssl-mode=none"
-  //          );
-  //      dbConnection.Open();
-		//IDbCommand dbCommand = dbConnection.CreateCommand();
-  //      dbCommand.CommandText = "select id,nombre from categoria order by id";
-  //      IDataReader dataReader = dbCommand.ExecuteReader();
 
+
+		CellRendererText cellRendererText = new CellRendererText();
+		//treeView.AppendColumn("ID", cellRendererText, delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+		//{
+		//	//Categoria categoria=(Categoria) tree_model.GetValue(iter, 0); 
+		//	//cellRendererText.Text = categoria.Id + "";
+
+		//	object model = tree_model.GetValue(iter, 0);
+		//	object value = model.GetType().GetProperty("Id").GetValue(model);
+		//	cellRendererText.Text = value + "";
+
+		//});
+
+
+		//treeView.AppendColumn("Nombre", cellRendererText, delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+   //     {
+			////Categoria categoria=(Categoria) tree_model.GetValue(iter, 0); 
+			////cellRendererText.Text = categoria.Nombre + "";
+
+			//object model = tree_model.GetValue(iter, 0);
+			//object value = model.GetType().GetProperty("Nombre").GetValue(model);
+			//cellRendererText.Text = value + "";
+
+        //});
+
+		string[] properties = new string[] { "Id", "Nombre" };
+		foreach(string property in properties){
+			string propertyName = property;
+			treeView.AppendColumn("ID", cellRendererText, delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+        {
+            //Categoria categoria=(Categoria) tree_model.GetValue(iter, 0); 
+            //cellRendererText.Text = categoria.Id + "";
+
+            object model = tree_model.GetValue(iter, 0);
+				object value = model.GetType().GetProperty(property).GetValue(model);
+            cellRendererText.Text = value + "";
+
+        });
+
+		}
+
+
+
+		//treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
+		ListStore listStore = new ListStore(typeof(Categoria));
+		treeView.Model = listStore;
 
         
+		while (dataReader.Read())
+			listStore.AppendValues(new Categoria((ulong)dataReader["id"], (string)dataReader["nombre"]));
+        //    Console.WriteLine("id={0} nombre={1}", dataReader["id"], dataReader["nombre"]);
+        //Console.WriteLine("id={0} nombre={1}", dataReader[0], dataReader[1]);
+        
+        dataReader.Close();
 
-
-		//treeView.AppendColumn("ID", new CellRendererText(), "text", 0);
-		//treeView.AppendColumn("Nombre", new CellRendererText(), "text", 1);
-		//ListStore listStore = new ListStore(typeof(string), typeof(string));
-		//treeView.Model = listStore;
-		////listStore.AppendValues(dataReader[0], dataReader[1]);
-		////listStore.AppendValues(dataReader[0], dataReader[1]);
-
-		//while (dataReader.Read())
-		//	listStore.AppendValues(dataReader["id"].ToString(), dataReader["nombre"].ToString());
-  //      //    Console.WriteLine("id={0} nombre={1}", dataReader["id"], dataReader["nombre"]);
-  //      //Console.WriteLine("id={0} nombre={1}", dataReader[0], dataReader[1]);
-
-  //      dataReader.Close();
-
-		//dbConnection.Close();
+		dbConnection.Close();
 
     }
 
