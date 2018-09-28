a4ac561b4d7254aff78956de3f40c30c243dded0
sergioosma09 <sergioosma09@gmail.com>
2018-09-28 09:20:14 +0200

    CCategoria: añadodp App con Singleton y ventana CCategoriaWindow

--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -8,75 +8,78 @@ using System.Reflection;
 
 public partial class MainWindow : Gtk.Window
 {
-	private IDbConnection dbConnection;
+
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
-        Build();
-		dbConnection = new MySqlConnection(
-                "server=localhost; database=dbprueba;user=root;password=sistemas;ssl-mode=none"
-            );
-        dbConnection.Open();
+	{
+		Build();
+		App.Instance.DbConnection = new MySqlConnection(
+				"server=localhost; database=dbprueba;user=root;password=sistemas;ssl-mode=none"
+			);
+		new CategoriaWindow();
+
+		App.Instance.DbConnection.Open();
 
 		//insert();
 		//update();
 		update(new Categoria(3, "categoria 3" + DateTime.Now));
-		delete();
+		//delete();
+
         
-      
 		CellRendererText cellRendererText = new CellRendererText();
-	
+
 
 		string[] properties = new string[] { "Id", "Nombre" };
-		foreach(string property in properties){
-			string propertyName = property;
+		foreach (string property in properties)
+		{
 			treeView.AppendColumn(property, cellRendererText, delegate (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter)
-        {            
-            object model = tree_model.GetValue(iter, 0);
-				object value = model.GetType().GetProperty(property).GetValue(model);
-            cellRendererText.Text = value + "";
+		{
+			object model = tree_model.GetValue(iter, 0);
+			object value = model.GetType().GetProperty(property).GetValue(model);
+			cellRendererText.Text = value + "";
 
-        });
+		});
 
 		}
-      
+
 		ListStore listStore = new ListStore(typeof(Categoria));
 		treeView.Model = listStore;
 
-		IDbCommand dbCommand = dbConnection.CreateCommand();
-        dbCommand.CommandText = "select id,nombre from categoria order by id";
-        IDataReader dataReader = dbCommand.ExecuteReader();
+		IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
+		dbCommand.CommandText = "select id,nombre from categoria order by id";
+		IDataReader dataReader = dbCommand.ExecuteReader();
 		while (dataReader.Read())
 			listStore.AppendValues(new Categoria((ulong)dataReader["id"], (string)dataReader["nombre"]));
 
-        
-        dataReader.Close();
 
-		dbConnection.Close();
+		dataReader.Close();
 
-    }
+
+	}
 
 	private void insert()
 	{
-		IDbCommand dbCommand = dbConnection.CreateCommand();
+		IDbCommand dbCommand =App.Instance.DbConnection.CreateCommand();
 		dbCommand.CommandText = "insert into categoria (nombre) values ('categoria 4')";
 		int filas=dbCommand.ExecuteNonQuery();
 	}
 
 	private void update()
     {
-        IDbCommand dbCommand = dbConnection.CreateCommand();
+		IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
         dbCommand.CommandText = "update categoria set nombre='categoria 4 modificada' where id =4";      
 		dbCommand.ExecuteNonQuery();
     }
 	private void update(Categoria categoria)
     {
-        IDbCommand dbCommand = dbConnection.CreateCommand();
+		IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
 		dbCommand.CommandText = "update categoria set nombre=@nombre where id=@id";
+
 		IDbDataParameter dbDataParameterNombre = dbCommand.CreateParameter();
         dbDataParameterNombre.ParameterName = "nombre";
         dbDataParameterNombre.Value = categoria.Nombre;
         dbCommand.Parameters.Add(dbDataParameterNombre);
-        IDbDataParameter dbDataParameterId = dbCommand.CreateParameter();
+        
+		IDbDataParameter dbDataParameterId = dbCommand.CreateParameter();
         dbDataParameterId.ParameterName = "id";
         dbDataParameterId.Value = categoria.Id;
         dbCommand.Parameters.Add(dbDataParameterId);
@@ -85,13 +88,14 @@ public partial class MainWindow : Gtk.Window
 
 	private void delete()
     {
-        IDbCommand dbCommand = dbConnection.CreateCommand();
+		IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
         dbCommand.CommandText = "delete from categoria where id=4";
         dbCommand.ExecuteNonQuery();
     }
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
-        Application.Quit();
+		App.Instance.DbConnection.Close();
+		Application.Quit();
         a.RetVal = true;
     }
 }
