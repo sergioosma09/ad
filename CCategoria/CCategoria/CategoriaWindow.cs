59d558b9b0fcbcab29ec281b189497355c9b448a
sergioosma09 <sergioosma09@gmail.com>
2018-09-28 09:37:50 +0200

    CCategoria : añadido dbCommandHelper con addParameter

--- CCategoria/CCategoria/CategoriaWindow.cs
+++ CCategoria/CCategoria/CategoriaWindow.cs
@@ -16,14 +16,10 @@ namespace CCategoria
 				  IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
                   dbCommand.CommandText = "insert into categoria (nombre) values (@nombre)";
 
-                  IDbDataParameter dbDataParameter = dbCommand.CreateParameter();
-                  dbDataParameter.ParameterName = "nombre";
-                  dbDataParameter.Value = entryNombre.Text;
-                  dbCommand.Parameters.Add(dbDataParameter);
+				  DbCommandHelper.AddParameter(dbCommand, "nombre", entryNombre.Text);
 
                   int filas = dbCommand.ExecuteNonQuery();
 
-                  Console.WriteLine("Nombre = " + entryNombre.Text);
 			};
 
 		}
