44b90b962b3fef3aa24391a5ccf759e40ebda2c9
sergioosma09 <sergioosma09@gmail.com>
2018-12-07 10:42:13 +0100

    Añadido CategoriaConsole, algunos actions y falta añadir el tryAction

--- GCategoria/src/main/java/serpis/ad/CategoriaDao.java
+++ GCategoria/src/main/java/serpis/ad/CategoriaDao.java
@@ -5,47 +5,51 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class CategoriaDao {
-	private static void insert(Categoria categoria) throws SQLException {
+	private static int insert(Categoria categoria) throws SQLException {
 	String insertSql = "insert into categoria (nombre) values (?)";
 	PreparedStatement preparedStatement = App.getInstance().getConnection().prepareStatement(insertSql);
 	preparedStatement.setObject(1, categoria.getNombre());
-	preparedStatement.executeUpdate();
-	preparedStatement.close();
+	return preparedStatement.executeUpdate();
 }
 
-private static void delete(int id) {
+public static int delete(long id) throws SQLException {
 	String deleteSql = "delete from categoria where id=?";
 	PreparedStatement preparedStatement;
-	try {
-		preparedStatement = App.getInstance().getConnection().prepareStatement(deleteSql);
-		preparedStatement.setInt(1, id);
-		preparedStatement.executeUpdate();
-		preparedStatement.close();
-	} catch (SQLException e) {
-		System.out.println(e);
-		System.out.println("No se puede por el foreign key");
+	preparedStatement = App.getInstance().getConnection().prepareStatement(deleteSql);
+	preparedStatement.setLong(1, id);
+	return preparedStatement.executeUpdate();
 
-	}
 }
 
-private static void Load() throws SQLException {
+public static Categoria load(long id) throws SQLException {
 
 	String selectSql = "select * from categoria";
 	Statement statement = App.getInstance().getConnection().createStatement();
 	ResultSet resultSet = statement.executeQuery(selectSql);
 	while (resultSet.next())
 		System.out.printf("%s %s\n", resultSet.getObject(1), resultSet.getObject(2));
-	statement.close(); // implicit resultset.close()
+	return (Categoria) resultSet;
+	
 
 }
 
-private static void Update() throws SQLException {
+private static int update(Categoria categoria) throws SQLException {
 	String updateSql = "update categoria set nombre=@nombre where id=@id";
 	PreparedStatement preparedStatement = App.getInstance().getConnection().prepareStatement(updateSql);
-	preparedStatement.executeUpdate();
-	preparedStatement.close();
+	return preparedStatement.executeUpdate();
+}
+
+public static int save (Categoria categoria) throws SQLException{
+	if(categoria.getId()==0)
+		return insert(categoria);
+	else
+		return update(categoria);
+}
+public static List<Categoria> getAll() throws SQLException{
+	return null;
 }
 	
 	
--- GCategoria/src/main/java/serpis/ad/GCategoriaMain.java
+++ GCategoria/src/main/java/serpis/ad/GCategoriaMain.java
@@ -18,8 +18,7 @@ public class GCategoriaMain {
 
 	public static void main(String[] args) throws SQLException {
 		App.getInstance().setConnection(DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas"));
-		Menu.create("Menú Categoría").exitWhen("0 - Salir").add("1 - Nuevo", GCategoriaMain::nuevo).add("2 - Editar",
-				GCategoriaMain::editar).loop();
+		Menu.create("Menú Categoría").add("1 - Nuevo",GCategoriaMain::nuevo).add("2 - Editar", GCategoriaMain::editar).exitWhen("3 - Salir").loop();
 		App.getInstance().getConnection().close();
 
 //	List<Action> actions = new ArrayList<>();
@@ -37,17 +36,54 @@ public class GCategoriaMain {
 	}
 
 	public static void nuevo() {
-		System.out.println("Método nuevo");
+		Categoria categoria= new Categoria();
+		CategoriaConsole.newCategoria(categoria);
+		try{
+			
+			CategoriaDao.save(categoria);
+		}catch(Exception e) {
+			
+		}
 	}
 
-	public static void editar() {
-		System.out.println("Método editado");
-		int id = ScannerHelper.getInt("Id: ");
+	public static void editar(){	
+		long id= CategoriaConsole.getId();
+		
+		try {
+			Categoria categoria=CategoriaDao.load(id);
+			if(categoria==null) {
+				CategoriaConsole.idNotExists();
+				return;
+			}
+			CategoriaConsole.editCategoria(categoria);		
+			CategoriaDao.save(categoria);
+				
+		}catch(Exception e) {
+			
+		}
+		
 	}
-
-
 	
+	public static void eliminar() throws SQLException {
+		long id= CategoriaConsole.getId();
+		if(CategoriaConsole.deleteConfirm())
+			CategoriaDao.delete(id);
 
-
+	}
+	
+	public static void consultar() throws SQLException{
+		long id= CategoriaConsole.getId();
+		Categoria categoria=CategoriaDao.load(id);
+		if(categoria==null) {
+			CategoriaConsole.idNotExists();
+			return;
+		}
+		CategoriaConsole.show(categoria);
+	}
+	
+	public static void listar() throws SQLException{
+		List<Categoria> categorias=CategoriaDao.getAll();
+		CategoriaConsole.showList(categorias);
+	}
 
 }
